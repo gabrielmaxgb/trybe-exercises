a32897dba880a312b5e2e5be35d84217c5105bec
Max <gmaxgomes@gmail.com>
2020-11-04 18:28:22 -0300

    Exercicio 1

--- BLOCO_5/5.3/script.js
+++ BLOCO_5/5.3/script.js
@@ -15,4 +15,29 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
 
-  
+/* Exercício 1 */
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createMonthDays() {
+    let daysList = document.getElementById('days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        let dayListItem = document.createElement('li');
+
+        if (dezDaysList[index] === 24 || dezDaysList[index] === 31) {
+            dayListItem.className = 'day holiday';
+        }
+
+        if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18) {
+            dayListItem.className = 'day friday';
+        }
+
+        if (dezDaysList[index] === 25) {
+            dayListItem.className = 'day holiday friday';
+        }
+        dayListItem.innerHTML = dezDaysList[index];
+        daysList.appendChild(dayListItem);
+    }
+};
+
+createMonthDays();
