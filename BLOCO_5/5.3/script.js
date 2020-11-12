6ed251c8e13a089d511d23bb29092f3338f801bd
Max <gmaxgomes@gmail.com>
2020-11-12 16:01:15 -0300

    att

--- BLOCO_5/5.3/script.js
+++ BLOCO_5/5.3/script.js
@@ -1,215 +1,263 @@
 function createDaysOfTheWeek() {
     const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
     const weekDaysList = document.querySelector('.week-days');
-  
+
     for (let index = 0; index < weekDays.length; index += 1) {
       const days = weekDays[index];
       const dayListItem = document.createElement('li');
       dayListItem.innerHTML = days;
-  
+
       weekDaysList.appendChild(dayListItem);
     };
   };
-  
+
   createDaysOfTheWeek();
-  
+
   // Escreva seu código abaixo.
 
 /* Exercício 1 */
-const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
-function createMonthDays() {
-    let daysList = document.getElementById('days');
+/* Exercício 1 */
+function setMonthDays() {
 
-    for (let index = 0; index < dezDaysList.length; index += 1) {
-        let dayListItem = document.createElement('li');
+  let listOfDays = document.getElementById('days'); // ul
 
-        if (dezDaysList[index] === 24 || dezDaysList[index] === 31) {
-            dayListItem.className = 'day holiday';
-        } else if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18) {
-            dayListItem.className = 'day friday';
-        }else if (dezDaysList[index] === 25) {
-            dayListItem.className = 'day holiday friday';
-        } else {
-            dayListItem.className = 'day';
-        }
-        dayListItem.innerHTML = dezDaysList[index];
-        daysList.appendChild(dayListItem);
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    //Creates element li
+    let listItem = document.createElement('li');
+    listItem.innerHTML = dezDaysList[index];
+    listOfDays.appendChild(listItem);
+    listItem.className = 'day';
+
+    if (dezDaysList[index] === 24 || dezDaysList[index] === 31) {
+      listItem.className = 'day holiday';
     }
+    if(dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18) {
+      listItem.className = 'day friday';
+    }
+    if (dezDaysList[index] === 25) {
+      listItem.className = 'day holiday friday';
+    }
+  }
 };
-createMonthDays();
+setMonthDays();
 
 /* Exercício 2 */
-function createsHolidayButton(string) {
-    let getButtonContainer = document.querySelector('.buttons-container');
-    let newButton = document.createElement('button');
-    let newButtonId = 'btn-holiday';
+function setHolidayButton(string) {
 
-    newButton.id = newButtonId;
-    newButton.innerHTML = string;
-    getButtonContainer.appendChild(newButton);
-}
-createsHolidayButton('Feriados');
+  let getButtonsContainer = document.querySelector('.buttons-container');
+  let hollidayButton = document.createElement('button');
+
+  hollidayButton.innerHTML = string;
+  hollidayButton.id = 'btn-holiday';
+  getButtonsContainer.appendChild(hollidayButton);
+
+};
+setHolidayButton('Feriados');
 
 /* Exercício 3 */
-function changeBgColor() {
-    let holidayButton = document.getElementById('btn-holiday');
-    let holidays = document.querySelectorAll('.holiday');
-    let backgroundColor = 'white';
-    let newBackgroundColor = 'green';
+function changeBgColorOfHoliday() {
 
-    holidayButton.addEventListener('click', function () {
+  let getHolidays = document.querySelectorAll('.holiday'); // returns holidays
+  let hollidayButton = document.getElementById('btn-holiday');
+  let calendarColor = 'rgb(238,238,238)';
+  let newColor = 'white';
 
-        for (let index = 0; index < holidays.length; index += 1) {
-            if (holidays[index].style.backgroundColor == newBackgroundColor) {
-                holidays[index].style.backgroundColor = backgroundColor;
-            } else {
-                holidays[index].style.backgroundColor = newBackgroundColor;
-            }
-        }
-    });
-}
-changeBgColor();
+  hollidayButton.addEventListener('click', function () {
+
+    for (let index = 0; index < getHolidays.length; index += 1) { //percorre holidays mudando a cor
+
+      let holiday = getHolidays[index];
+
+      if (holiday.style.backgroundColor == newColor) {
+        holiday.style.backgroundColor = calendarColor;
+      } else {
+        holiday.style.backgroundColor = newColor;
+      }
+
+    }
+  });
+};
+changeBgColorOfHoliday();
 
 /* Exercício 4 */
-function createsFridayButton(friday) {
-    let buttonsContainer = document.querySelector('.buttons-container');
-    let newButton = document.createElement('button');
-    let newButtonID = 'btn-friday';
+function setFridayButton(string) {
 
-    newButton.id = newButtonID;
-    newButton.innerHTML = friday;
-    buttonsContainer.appendChild(newButton);
+  let getButtonsContainer = document.querySelector('.buttons-container');
+  let fridayButton = document.createElement('button');
+
+  fridayButton.innerHTML = string;
+  fridayButton.id = 'btn-friday';
+  getButtonsContainer.appendChild(fridayButton);
+
 }
-createsFridayButton('Sexta-feira');
+setFridayButton('Sexta-feira');
 
 /* Exercício 5 */
-function changeFridayText(theFridays) {
+function changeFridayInnerText(string){
 
-    let fridayButton = document.getElementById('btn-friday');
-    let fridays = document.querySelectorAll('.friday');
+  let getFridays = document.getElementsByClassName('friday'); //returns fridays elements
+  let fridayButton = document.getElementById('btn-friday');
 
-    fridayButton.addEventListener('click', function () {
+  fridayButton.addEventListener('click', function () {
+
+    for (let index = 0; index < getFridays.length; index += 1) {
+
+      let getFridaysNum = document.getElementsByClassName('friday')[index].innerHTML;
+
+      if (getFridays[index].innerHTML == string) {
+        getFridays[index].innerHTML = arrFriday[index];
+      } else {
+        getFridays[index].innerHTML = string;
+      }
 
 
-        for (let index = 0; index < fridays.length; index += 1) {
 
-            if (fridays[index].innerText != 'Sexta-feira') {
-                fridays[index].innerText = 'Sexta-feira';
-            } else {
-                fridays[index].innerText = theFridays[index];
-            }
-        }
-    })
+    }
+
+  });
+
 }
-let theFridays = [ 4, 11, 18, 25 ];
-changeFridayText(theFridays);
+arrFriday = [ 4, 11, 18, 25 ];
+changeFridayInnerText('Sextoooou');
 
 /* Exercício 6 */
-function mouseOver() {
-    let day = document.getElementById('days');
+function zoomIn() {
 
-    day.addEventListener('mouseover', function(event) {
-      event.target.style.fontSize = '40px';
-      event.target.style.fontWeight = 'bold';
-    })
-  };
-function mouseOut() {
-    let day = document.getElementById('days');
+  let days = document.querySelector('#days');
 
-    day.addEventListener('mouseout', function(event) {
-      event.target.style.fontWeight = '200';
-      event.target.style.fontSize = '20px';
-    })
-  };
-mouseOver();
-mouseOut();
+  days.addEventListener('mouseover', function (event) {
+
+    event.target.style.fontWeight = 'bold';
+
+  });
+
+}
+
+function zoomOut() {
+  let days = document.querySelector('#days');
+
+  days.addEventListener('mouseout', function(event) {
+    event.target.style.fontWeight = '';
+  })
+};
+
+zoomIn();
+zoomOut();
 
 /* Exercício 7 */
 function addTask(task) {
-    let myTasks = document.querySelector('.my-tasks');
-    let tarefa = document.createElement('span');
-    tarefa.innerHTML = task;
-    myTasks.appendChild(tarefa);
+
+  let myTasks = document.querySelector('.my-tasks');
+  let newTask = document.createElement('span');
+
+  newTask.innerHTML = task;
+  myTasks.appendChild(newTask);
 }
-addTask('cozinhar');
+addTask('Estudar');
 
 /* Exercício 8 */
-function addsSubtitle(color) {
-    let myTasks = document.querySelector('.my-tasks');
-    let subtitle = document.createElement('div');
+function addTaskColor(color) {
 
-    subtitle.className = 'task';
-    subtitle.style.backgroundColor = color;
-    myTasks.appendChild(subtitle);
-}
-addsSubtitle('green');
+  let myTasks = document.querySelector('.my-tasks');
+  let subtitle = document.createElement('div');
 
-/* Exercício 9  */
-function addEvent() {
-
-    let currentTask = document.getElementsByClassName('task selected');
-    let myTasks = document.querySelector('.task');
-
-    myTasks.addEventListener('click', function (event) {
-
-        if (currentTask.length === 0) {
-            event.target.className = 'task selected';
-          } else {
-            event.target.className = 'task';
-          }
-    });
+  subtitle.className = 'task';
+  subtitle.style.backgroundColor = color;
+  myTasks.appendChild(subtitle);
 
 }
-addEvent();
+addTaskColor('green');
 
-/* Exercício 10 */           //https://app.betrybe.com/course/fundamentals/javascript/dom-manipulation/js-part-7-solutions
-function colorDay() {
-    let selectedTask = document.getElementsByClassName('task selected');
-    let day = document.getElementById('days');
-    let taskDiv = document.querySelector('.task');
-    let taskColor = taskDiv.style.backgroundColor;
+/* Exercício 9 */
 
-    day.addEventListener('click', function(event){
-      let eventTargetColor = event.target.style.color;
-      if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
-        let color = selectedTask[0].style.backgroundColor;
-        event.target.style.color = color;
-      } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
-        event.target.style.color = 'rgb(119,119,119)';
-      }
-    });
-  };
+function selectTask() {
 
-  colorDay();
+  let task = document.querySelector('.task');
 
-  /* Bônus */            //https://app.betrybe.com/course/fundamentals/javascript/dom-manipulation/js-part-7-solutions
-  function addNewTask() {
-    let getInputField = document.querySelector('#task-input');
-    let addInputButton = document.querySelector('#btn-add');
-    let getTaskList = document.querySelector('.task-list');
+  task.addEventListener('click', function (event) {
+
+    if (event.target.className == 'task selected') {
+      event.target.className = 'task';
+    } else {
+      event.target.className = 'task selected';
+    }
+  });
+}
+selectTask();
+
+/* Exercício 10 */  // ENTENDER ISSO AQUI
+
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName('task selected');
+  let days = document.querySelector('#days');
+  let taskDiv = document.querySelector('.task');
+  let taskColor = taskDiv.style.backgroundColor;
   
-    addInputButton.addEventListener('click', function() {
-      if (getInputField.value.length > 0) {
-        let newLi = document.createElement('li');
-        newLi.innerText = getInputField.value;
-  
-        getTaskList.appendChild(newLi);
-        getInputField.value = '';
-      } else {
-        alert('Error: Digite ao menos 1 caractere.');
-      }
-    })
-  
-    getInputField.addEventListener('keyup', function(event) {
-      if (event.keyCode === 13 && getInputField.value.length > 0) {
-        let newLi = document.createElement('li');
-        newLi.innerText = getInputField.value;
-  
-        getTaskList.appendChild(newLi);
-        getInputField.value = '';
-      }
-    });
-  };
-  
-  addNewTask();
+  days.addEventListener('click', function(event){
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = 'rgb(119,119,119)';
+    }
+  });
+};
+
+setDayColor();
+/*function setDayColorTask() {
+
+  let day = document.querySelector('.day');
+  let dayColor = day.style.color;
+  let taskColor = document.querySelector('.task', '.selected').style.backgroundColor;
+
+  day.addEventListener('click', function () {
+
+    if (dayColor != taskColor) {
+      daycolor = taskColor;
+    } else {
+      dayColor = 'rgb(119,119,119)';
+    }
+
+  });
+
+}
+setDayColorTask();*/
+
+/* Bônus */       // ENTENDER ISSO AQUI
+
+/*Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
+Se nenhum caractere for inserido no campo input, a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".
+Ao pressionar a tecla "enter" o evento também deverá ser disparado.*/
+
+function addNewTask() {
+  let getInputField = document.querySelector('#task-input');
+  let addInputButton = document.querySelector('#btn-add');
+  let getTaskList = document.querySelector('.task-list');
+
+  addInputButton.addEventListener('click', function() {
+    if (getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    } else {
+      alert('Error: Digite ao menos 1 caractere.');
+    }
+  })
+
+  getInputField.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13 && getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    }
+  });
+};
+
+addNewTask();
