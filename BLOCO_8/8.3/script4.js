65acd5615d126bb712e7b6966c6177659fff2b5e
Max <gmaxgomes@gmail.com>
2020-12-02 18:15:04 -0300

    ex 4

--- BLOCO_8/8.3/script4.js
+++ BLOCO_8/8.3/script4.js
@@ -77,6 +77,7 @@ const expectedResult = {
 
 function longestNamedBook() {
   // escreva seu código aqui
+  return books.reduce((biggestBook, actualBook) => (actualBook.name.length > biggestBook.name.length) ? actualBook : biggestBook);
 }
 
 assert.deepEqual(longestNamedBook(), expectedResult);
